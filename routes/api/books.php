93bed8135f60c4c02139e477ec85a1f1655b0e93
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2026-04-21 19:42:13 -0300

    refactor: removing unnecessary files

--- routes/api/books.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\BookController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('books/search', [BookController::class, 'search']);
--- routes/api/reading-list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\ReadingListController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('reading-list', [ReadingListController::class, 'index']);
-Route::post('reading-list', [ReadingListController::class, 'store']);
-Route::patch('reading-list/{book}', [ReadingListController::class, 'update']);
-Route::delete('reading-list/{book}', [ReadingListController::class, 'destroy']);
